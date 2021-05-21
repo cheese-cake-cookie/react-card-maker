cdd76cf6b941e2bdfc66840549d6cf1e173b3576
wasabi <wasabi@podbbang.com>
2021-05-21 14:25:43 +0900

    feat: Add github login

---
+++ src/Login.jsx
@@ -0,0 +1,11 @@
+import styles from './Login.module.css';
+
+function Login() {
+  return (
+    <div className={styles.login__container}>
+      <h1>Welcome to My Awesome App</h1>
+    </div>
+  );
+}
+
+export default Login;
